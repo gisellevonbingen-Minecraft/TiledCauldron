a096f6d290abf968fcbdab4acbd6beecfe4c1c82
gisellevonbingen <gisellevonbingen@gmail.com>
2022-11-15 14:29:41 +0900

    BlockEntityType#isValid return be 'block is AbstractCauldron'

--- src/main/java/gisellevonbingen/tiled_cauldron/common/registries/ModBlockEntityTypes.java
+++ src/main/java/gisellevonbingen/tiled_cauldron/common/registries/ModBlockEntityTypes.java
@@ -2,8 +2,9 @@ package gisellevonbingen.tiled_cauldron.common.registries;
 
 import gisellevonbingen.tiled_cauldron.common.TiledCauldron;
 import gisellevonbingen.tiled_cauldron.common.tile.CauldronBlockEntity;
-import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.AbstractCauldronBlock;
 import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -12,5 +13,13 @@ public class ModBlockEntityTypes
 {
 	public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITIES, TiledCauldron.MODID);
 
-	public static final RegistryObject<BlockEntityType<?>> CAULDRON = BLOCK_ENTITY_TYPES.register("cauldron", () -> BlockEntityType.Builder.of(CauldronBlockEntity::new, Blocks.CAULDRON).build(null));
+	public static final RegistryObject<BlockEntityType<?>> CAULDRON = BLOCK_ENTITY_TYPES.register("cauldron", () -> new BlockEntityType<>(CauldronBlockEntity::new, null, null)
+	{
+		@Override
+		public boolean isValid(BlockState state)
+		{
+			return state.getBlock() instanceof AbstractCauldronBlock;
+		}
+
+	});
 }
