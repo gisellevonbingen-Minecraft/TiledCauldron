b7361084ee25eae24e70ad26b86badffbd6284fe
gisellevonbingen <gisellevonbingen@gmail.com>
2023-06-14 14:05:08 +0900

    Port to 1.20

--- src/main/java/gisellevonbingen/tiled_cauldron/common/tile/CauldronBlockEntity.java
+++ src/main/java/gisellevonbingen/tiled_cauldron/common/tile/CauldronBlockEntity.java
@@ -11,8 +11,8 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Fluid;
 import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.common.util.LazyOptional;
-import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
 
 public class CauldronBlockEntity extends BlockEntity
 {
@@ -47,7 +47,7 @@ public class CauldronBlockEntity extends BlockEntity
 	@Override
 	public <T> LazyOptional<T> getCapability(Capability<T> cap, Direction side)
 	{
-		if (cap == CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY)
+		if (cap == ForgeCapabilities.FLUID_HANDLER)
 		{
 			return LazyOptional.of(this::getFluidTank).cast();
 		}
