2cae37f1ae9336fc32fdf9895dc2ae4bbf798bd1
Michael Chrost <michael@chrost.dk>
2026-02-23 18:10:23 +0100

    1.21.11

--- src/main/java/gisellevonbingen/tiled_cauldron/common/TiledCauldron.java
+++ src/main/java/gisellevonbingen/tiled_cauldron/common/TiledCauldron.java
@@ -1,31 +1,33 @@
 package gisellevonbingen.tiled_cauldron.common;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import gisellevonbingen.tiled_cauldron.common.registries.ModBlockEntityTypes;
-import gisellevonbingen.tiled_cauldron.common.tile.CauldronBlockEntity;
-import net.minecraft.resources.Identifier;
-import net.minecraft.world.level.block.Blocks;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.BucketItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.capabilities.Capabilities;
+import net.minecraft.world.level.block.Blocks;
 import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
+import gisellevonbingen.tiled_cauldron.common.capabilities.CauldronTank;
+import net.neoforged.neoforge.fluids.FluidType;
 
 @Mod(TiledCauldron.MODID)
 public class TiledCauldron
 {
 	public static final String MODID = "tiled_cauldron";
-	public static final Logger LOGGER = LogManager.getLogger();
 
 	public TiledCauldron()
 	{
 		IEventBus fml_bus = ModLoadingContext.get().getActiveContainer().getEventBus();
 		fml_bus.addListener(this::onCommonSetup);
 		fml_bus.addListener(this::onRegisterCapabilities);
-		ModBlockEntityTypes.BLOCK_ENTITY_TYPES.register(fml_bus);
+		NeoForge.EVENT_BUS.addListener(this::onRightClickBlock);
 	}
 
 	public void onCommonSetup(FMLCommonSetupEvent e)
@@ -40,13 +42,45 @@ public class TiledCauldron
 	{
 		e.registerBlock(Capabilities.Fluid.BLOCK, (level, pos, state, blockEntity, context) ->
 		{
-			return blockEntity instanceof CauldronBlockEntity cauldronBlockEntity ? cauldronBlockEntity.getFluidTank() : null;
+			return CauldronTank.get(level, pos);
 		}, Blocks.CAULDRON, Blocks.WATER_CAULDRON, Blocks.LAVA_CAULDRON, Blocks.POWDER_SNOW_CAULDRON);
 	}
 
-	public static Identifier rl(String path)
+	public void onRightClickBlock(PlayerInteractEvent.RightClickBlock e)
 	{
-		return Identifier.fromNamespaceAndPath(MODID, path);
+		ItemStack item = e.getItemStack();
+		Level level = e.getLevel();
+		BlockPos pos = e.getPos();
+		CauldronTank tank = CauldronTank.get(level, pos);
+		long amount = tank.getAmountAsLong(0);
+
+		if (item.getItem() != Items.BUCKET)
+		{
+			if (item.getItem() instanceof BucketItem bucket && amount > 0 && amount < FluidType.BUCKET_VOLUME && tank.getFluid().isEmpty() == false && tank.getFluid().getFluid() == bucket.content)
+			{
+				if (level.isClientSide() == true)
+				{
+					e.setCanceled(true);
+					return;
+				}
+
+				if (tank.fillFromBucket(bucket.content))
+				{
+					if (e.getEntity().getAbilities().instabuild == false)
+					{
+						e.getEntity().setItemInHand(e.getHand(), new ItemStack(Items.BUCKET));
+					}
+					e.setCanceled(true);
+				}
+			}
+
+			return;
+		}
+
+		if (amount > 0 && amount < FluidType.BUCKET_VOLUME)
+		{
+			e.setCanceled(true);
+		}
 	}
 
 }
